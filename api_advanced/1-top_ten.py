9836ad976a15fcb72c50ade55fdf91bc2158043b
Cyusadelphin <d.cyusa@alustudent.com>
2024-11-17 02:37:56 -0800

    hello

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,18 @@
-
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        print(None)
-
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")] 
